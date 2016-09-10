a127d14a60daee6a636d487daf7e8d32755e467c
alexandrosvas@hotmail.com <alexandrosvas@hotmail.com>
2016-09-10 20:50:02 +0300

    Updated Version

--- Hadoop_Implementation/src/main/java/Hadoop_Implementation_class.java
+++ Hadoop_Implementation/src/main/java/Hadoop_Implementation_class.java
@@ -636,7 +636,7 @@ public class Hadoop_Implementation_class
             //an trexoume se pseudodistributed xwris pragmatiko hdfs trexoume to parakatw
             Configuration conf = new Configuration();
             FileSystem fs = FileSystem.get(conf);
-            Path inFile = new Path("Hadoop_Implementation/hdfs_in/afg_adm_shp.ttl");
+            Path inFile = new Path(System.getProperty("user.dir")+"/Hadoop_Implementation/hdfs_in/afg_adm_shp.ttl");
             FSDataInputStream in = fs.open(inFile);
             RMLMapping mapping = RMLMappingFactory.extractRMLMapping(in);
            // RMLMapping mapping = RMLMappingFactory.extractRMLMapping("Hadoop_Implementation/hdfs_in/afg_adm_shp.ttl");
@@ -712,9 +712,9 @@ public class Hadoop_Implementation_class
 
 
             //local
-            FileInputFormat.addInputPath(job, new Path("Hadoop_Implementation/"+args[1]));
+            FileInputFormat.addInputPath(job, new Path(System.getProperty("user.dir")+"/Hadoop_Implementation/"+args[1]));
 //            //FileInputFormat.setInputDirRecursive(job,true);
-            FileOutputFormat.setOutputPath(job, new Path("Hadoop_Implementation/"+args[2]));
+            FileOutputFormat.setOutputPath(job, new Path(System.getProperty("user.dir")+"/Hadoop_Implementation/"+args[2]));
 
             //hdfs
 //            FileInputFormat.addInputPath(job, new Path("hdfs://hadoop-p2-1:9000/"+args[1]));

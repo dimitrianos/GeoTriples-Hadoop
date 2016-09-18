f9f9261dd419a558513cae7178ce5f74a7e35fdc
alexandrosvas@hotmail.com <alexandrosvas@hotmail.com>
2016-09-18 21:41:06 +0300

    Updated Version

--- Shapefile/src/main/java/com/esri/io/PointMultiPolygonWritable.java
+++ Shapefile/src/main/java/com/esri/io/PointMultiPolygonWritable.java
@@ -36,53 +36,53 @@ public class PointMultiPolygonWritable implements Writable{
         
 
         //create a multipolygon
-        JtsMultiPolygon jtsmlpl = new JtsMultiPolygon();
-        if(jtsmlpl.my_multi_polygon()!=null) {
-
-            multiPolygon = jtsmlpl.my_multi_polygon();
-
-        }
-        else
-        {
-
-            GeometryFactory gf_polygon_1 = new GeometryFactory();
-
-            Coordinate[] coords_1  =
-                    new Coordinate[] {new Coordinate(0, 0), new Coordinate(10, 0),
-                            new Coordinate(10, 10),  new Coordinate(0,0)};
-
-            LinearRing ring_1 = gf_polygon_1.createLinearRing( coords_1 );
-            LinearRing holes_1[] = null;
-
-
-            Polygon polygon_1 = gf_polygon_1.createPolygon(ring_1, holes_1);
-
-
-
-            GeometryFactory gf_polygon_2 = new GeometryFactory();
-
-            Coordinate[] coords_2  =
-                    new Coordinate[] {new Coordinate(20, 0), new Coordinate(30, 20),
-                            new Coordinate(30, 30),  new Coordinate(20,0)};
-
-            LinearRing ring_2 = gf_polygon_2.createLinearRing( coords_2 );
-            LinearRing holes_2[] = null;
-
-
-            Polygon polygon_2 = gf_polygon_2.createPolygon(ring_2, holes_2);
-
-
-            Polygon[] polygonSet = new Polygon[2];
-
-            polygonSet[0] = polygon_1;
-            polygonSet[1] = polygon_2;
-
-            GeometryFactory multi_factory = new GeometryFactory();
-
-            multiPolygon = multi_factory.createMultiPolygon(polygonSet);
-
-
-        }
+//        JtsMultiPolygon jtsmlpl = new JtsMultiPolygon();
+//        if(jtsmlpl.my_multi_polygon()!=null) {
+//
+//            multiPolygon = jtsmlpl.my_multi_polygon();
+//
+//        }
+//        else
+//        {
+//
+//            GeometryFactory gf_polygon_1 = new GeometryFactory();
+//
+//            Coordinate[] coords_1  =
+//                    new Coordinate[] {new Coordinate(0, 0), new Coordinate(10, 0),
+//                            new Coordinate(10, 10),  new Coordinate(0,0)};
+//
+//            LinearRing ring_1 = gf_polygon_1.createLinearRing( coords_1 );
+//            LinearRing holes_1[] = null;
+//
+//
+//            Polygon polygon_1 = gf_polygon_1.createPolygon(ring_1, holes_1);
+//
+//
+//
+//            GeometryFactory gf_polygon_2 = new GeometryFactory();
+//
+//            Coordinate[] coords_2  =
+//                    new Coordinate[] {new Coordinate(20, 0), new Coordinate(30, 20),
+//                            new Coordinate(30, 30),  new Coordinate(20,0)};
+//
+//            LinearRing ring_2 = gf_polygon_2.createLinearRing( coords_2 );
+//            LinearRing holes_2[] = null;
+//
+//
+//            Polygon polygon_2 = gf_polygon_2.createPolygon(ring_2, holes_2);
+//
+//
+//            Polygon[] polygonSet = new Polygon[2];
+//
+//            polygonSet[0] = polygon_1;
+//            polygonSet[1] = polygon_2;
+//
+//            GeometryFactory multi_factory = new GeometryFactory();
+//
+//            multiPolygon = multi_factory.createMultiPolygon(polygonSet);
+//
+//
+//        }
                   
     
     }

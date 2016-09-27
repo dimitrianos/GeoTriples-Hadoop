fc06c1884d2d5f66ba67d45a31bd76a3c651a55c
Dimitrianos Savva <dimis@di.uoa.gr>
2016-09-28 00:27:03 +0300

    Optimize reading coordinates for geometries. Use of a buffer.

--- Shapefile/src/main/java/com/esri/mapreduce/AbstractReader.java
+++ Shapefile/src/main/java/com/esri/mapreduce/AbstractReader.java
@@ -1,6 +1,7 @@
 package com.esri.mapreduce;
 
 import com.esri.shp.ShpReader;
+import java.io.IOException;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -11,13 +12,11 @@ import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
-import java.io.IOException;
-
 /**
  */
 abstract class AbstractReader<T extends Writable>
-        extends RecordReader<LongWritable, T>
-{
+        extends RecordReader<LongWritable, T> {
+
     protected final LongWritable m_recordNumber = new LongWritable();
     protected long m_length;
     protected FSDataInputStream m_shpStream;
@@ -26,35 +25,29 @@ abstract class AbstractReader<T extends Writable>
     @Override
     public void initialize(
             final InputSplit inputSplit,
-            final TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException
-    {
-        if (inputSplit instanceof FileSplit)
-        {
+            final TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
+        if (inputSplit instanceof FileSplit) {
             final FileSplit fileSplit = (FileSplit) inputSplit;
             m_length = fileSplit.getLength();
             final Path path = fileSplit.getPath();
             final FileSystem fileSystem = path.getFileSystem(taskAttemptContext.getConfiguration());
             m_shpStream = fileSystem.open(path);
+
             m_shpReader = new ShpReader(m_shpStream);
-        }
-        else
-        {
+        } else {
             throw new IOException("Input split is not an instance of FileSplit");
         }
     }
 
     @Override
-    public LongWritable getCurrentKey() throws IOException, InterruptedException
-    {
+    public LongWritable getCurrentKey() throws IOException, InterruptedException {
         return m_recordNumber;
     }
 
     @Override
-    public boolean nextKeyValue() throws IOException, InterruptedException
-    {
+    public boolean nextKeyValue() throws IOException, InterruptedException {
         final boolean hasMore = m_shpReader.hasMore();
-        if (hasMore)
-        {
+        if (hasMore) {
             next();
             m_recordNumber.set(m_shpReader.recordNumber);
         }
@@ -67,16 +60,13 @@ abstract class AbstractReader<T extends Writable>
     public abstract T getCurrentValue() throws IOException, InterruptedException;
 
     @Override
-    public float getProgress() throws IOException, InterruptedException
-    {
+    public float getProgress() throws IOException, InterruptedException {
         return m_shpStream.getPos() / m_length;
     }
 
     @Override
-    public void close() throws IOException
-    {
-        if (m_shpStream != null)
-        {
+    public void close() throws IOException {
+        if (m_shpStream != null) {
             m_shpStream.close();
             m_shpStream = null;
         }
--- Shapefile/src/main/java/com/esri/shp/ShpReader.java
+++ Shapefile/src/main/java/com/esri/shp/ShpReader.java
@@ -1,37 +1,26 @@
-
 package com.esri.shp;
 
-
-import com.esri.jts_extras.JtsMultiPolygon;
 import com.esri.jts_extras.PointorMultiPolygon;
-
-import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.CoordinateFilter;
-import com.vividsolutions.jts.geom.CoordinateSequence;
 import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.MultiPolygon;
-
-import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
-import org.apache.commons.io.EndianUtils;
-
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.io.EndianUtils;
 
 /**
  * http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
  */
-public class ShpReader implements Serializable
-{
+public class ShpReader implements Serializable {
+
     private transient DataInputStream m_dataInputStream;
     private transient ShpHeader m_shpHeader;
 
@@ -46,52 +35,48 @@ public class ShpReader implements Serializable
     public transient double xmax;
     public transient double ymax;
     public transient double mmin;
-    public transient double mmax;    
+    public transient double mmax;
     public transient int numParts;
     public transient int numPoints;
 
-    public  GeometryFactory multi_factory = new GeometryFactory();
-    public GeometryFactory gf=new GeometryFactory();
+    public GeometryFactory multi_factory = new GeometryFactory();
+    public GeometryFactory gf = new GeometryFactory();
 
+    byte[] buffer = new byte[32];
 
-    public ShpReader(final DataInputStream dataInputStream) throws IOException
-    {
+    public ShpReader(final DataInputStream dataInputStream) throws IOException {
         m_dataInputStream = dataInputStream;
         m_shpHeader = new ShpHeader(dataInputStream);
     }
 
-    public ShpHeader getHeader()
-    {
+    public ShpHeader getHeader() {
         return m_shpHeader;
     }
 
-    public boolean hasMore() throws IOException
-    {
+    public boolean hasMore() throws IOException {
         return m_dataInputStream.available() > 0;
     }
 
-    private void readRecordHeader() throws IOException
-    {
+    private void readRecordHeader() throws IOException {
         recordNumber = m_dataInputStream.readInt();
         contentLength = m_dataInputStream.readInt();
         contentLengthInBytes = contentLength + contentLength - 4;
 
-        shapeType = EndianUtils.readSwappedInteger(m_dataInputStream);
-        
+        m_dataInputStream.readFully(buffer, 0, 4);
+        shapeType = EndianUtils.readSwappedInteger(buffer, 0);
+
     }
 
     //a fuction that decides the geometry type of the record and sets the right values
-    public void PointorMultiPolygon_Selector(final Point point) throws IOException
-    {
-        
-        readRecordHeader();             
+    public void PointorMultiPolygon_Selector(final Point point) throws IOException {
+
+        readRecordHeader();
 
         //shapeType==1 => Point
-        if(shapeType==1)
-        {
-            
+        if (shapeType == 1) {
+
             point.apply(new CoordinateFilter() {
-        
+
                 @Override
                 public void filter(Coordinate coord) {
 
@@ -107,14 +92,13 @@ public class ShpReader implements Serializable
 
                     try {
 
-                        coord.y =EndianUtils.readSwappedDouble(m_dataInputStream);
+                        coord.y = EndianUtils.readSwappedDouble(m_dataInputStream);
 
                     } catch (IOException ex) {
 
                         Logger.getLogger(ShpReader.class.getName()).log(Level.SEVERE, null, ex);
 
                     }
-
 
                 }
 
@@ -123,55 +107,45 @@ public class ShpReader implements Serializable
             PointorMultiPolygon.setPointPnorMlPl(point);
             PointorMultiPolygon.setMultiPolygonPnorMlPl(null);
             PointorMultiPolygon.setMultiPolygonorPoint_Shapetype(shapeType);
-            
-            
-        }
-        //shapeType==5 => MultiPolygon
-        else if(shapeType==5)
-        {
+
+        } //shapeType==5 => MultiPolygon
+        else if (shapeType == 5) {
 
             readShapeHeader();
 
-            Polygon[] pll_list=new Polygon[numParts];
+            Polygon[] pll_list = new Polygon[numParts];
             //List<Polygon> pll_list = new ArrayList<Polygon>();
 
-            for (int i = 0, j = 1; i < numParts; ++i)
-            {
-
+            for (int i = 0, j = 1; i < numParts; ++i) {
 
                 final int count = m_parts[j++] - m_parts[i];
 
-                Coordinate[] coords  = new Coordinate[count];
+                Coordinate[] coords = new Coordinate[count];
 
+                for (int c = 0; c < count; c++) {
+                    m_dataInputStream.readFully(buffer, 0, 16);
 
-                for (int c = 0; c < count; c++)
-                {
-                    final double x = EndianUtils.readSwappedDouble(m_dataInputStream);
-                    final double y = EndianUtils.readSwappedDouble(m_dataInputStream);
-                    if (c > 0)
-                    {
-                        coords[c] =  new Coordinate(x,y);
-                    }
-                    else
-                    {
-                        coords[c] =  new Coordinate(x,y);
+                    final double x = EndianUtils.readSwappedDouble(buffer, 0);
+                    final double y = EndianUtils.readSwappedDouble(buffer, 8);
+
+                    if (c > 0) {
+                        coords[c] = new Coordinate(x, y);
+                    } else {
+                        coords[c] = new Coordinate(x, y);
                     }
                 }
-
-
 
                 //LineString lineString = gf.createLineString(coords);
 //System.out.println("geia sas");
-                LinearRing ring= null;
-                if( coords[coords.length-1].equals(coords[0])) {
+                LinearRing ring = null;
+                if (coords[coords.length - 1].equals(coords[0])) {
                     //if( lineString.isClosed() )
 
-                }
-                else {
-            //coords[coords.length-1]=coords[0];
+                } else {
+                    //coords[coords.length-1]=coords[0];
                     //CoordinateSequence sequence = lineString.getCoordinateSequence();
-                    coords = Arrays.copyOf(coords,coords.length+1);
-                    coords[coords.length-1]=coords[0];
+                    coords = Arrays.copyOf(coords, coords.length + 1);
+                    coords[coords.length - 1] = coords[0];
 //                    for( int n=0; n<sequence.size();n++){
 //
 //                        array[n] = sequence.getCoordinate(n);
@@ -187,16 +161,15 @@ public class ShpReader implements Serializable
 
                 Polygon polygon = gf.createPolygon(ring, holes);
 
-            //    polygons_list pol_l = new polygons_list();
-              //  pol_l.pll = polygon;
-                pll_list[i]=polygon;
+                //    polygons_list pol_l = new polygons_list();
+                //  pol_l.pll = polygon;
+                pll_list[i] = polygon;
 
             }
 
 //            Polygon[] polygonSet = new Polygon[pll_list.length];
 //
 //            polygonSet=pll_list(polygonSet);
-
 //            int k=0;
 //            for(polygons_list i : pll_list){
 //
@@ -205,56 +178,47 @@ public class ShpReader implements Serializable
 //                k++;
 //
 //            }
-
-
             MultiPolygon multiPolygon = new MultiPolygon(pll_list, multi_factory);
 
 //            JtsMultiPolygon mljtspl = new JtsMultiPolygon();
 //
 //            mljtspl.set_my_multi_polygon(multiPolygon);
-
             PointorMultiPolygon.setPointPnorMlPl(null);
             PointorMultiPolygon.setMultiPolygonPnorMlPl(multiPolygon);
             PointorMultiPolygon.setMultiPolygonorPoint_Shapetype(shapeType);
 
-
-        }
-        else
-        {
+        } else {
 
             PointorMultiPolygon.setPointPnorMlPl(null);
             PointorMultiPolygon.setMultiPolygonPnorMlPl(null);
             PointorMultiPolygon.setMultiPolygonorPoint_Shapetype(0);
 
         }
-            
+
     }
 
+    private void readShapeHeader() throws IOException {
+        m_dataInputStream.readFully(buffer, 0, 32);
+        xmin = EndianUtils.readSwappedDouble(buffer, 0);
+        ymin = EndianUtils.readSwappedDouble(buffer, 8);
+        xmax = EndianUtils.readSwappedDouble(buffer, 16);
+        ymax = EndianUtils.readSwappedDouble(buffer, 24);
 
-    private void readShapeHeader() throws IOException
-    {
-        xmin = EndianUtils.readSwappedDouble(m_dataInputStream);
-        ymin = EndianUtils.readSwappedDouble(m_dataInputStream);
-        xmax = EndianUtils.readSwappedDouble(m_dataInputStream);
-        ymax = EndianUtils.readSwappedDouble(m_dataInputStream);
+        m_dataInputStream.readFully(buffer, 0, 8);
+        numParts = EndianUtils.readSwappedInteger(buffer, 0);
+        numPoints = EndianUtils.readSwappedInteger(buffer, 4);
 
-        numParts = EndianUtils.readSwappedInteger(m_dataInputStream);
-        numPoints = EndianUtils.readSwappedInteger(m_dataInputStream);
-
-        if ((numParts + 1) > m_parts.length)
-        {
+        if ((numParts + 1) > m_parts.length) {
             m_parts = new int[numParts + 1];
         }
-        for (int p = 0; p < numParts; p++)
-        {
+        for (int p = 0; p < numParts; p++) {
             m_parts[p] = EndianUtils.readSwappedInteger(m_dataInputStream);
         }
         m_parts[numParts] = numPoints;
-    }
-    
 
-    
-    public class polygons_list{
+    }
+
+    public class polygons_list {
 
         private Polygon pll;
 
